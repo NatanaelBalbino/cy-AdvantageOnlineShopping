0f57f56993f173fc8269fbb4b934778907cd4d45
NatanaelBalbino <natanael.m.m.balbino@gmail.com>
2025-01-12 09:26:46 -0300

    Ajustado o envio do Uplaod e validação BDD

--- cypress/e2e/step_definitions/ValidateUpdateImageAPI/ValidateUpdateImageAPI.js
+++ cypress/e2e/step_definitions/ValidateUpdateImageAPI/ValidateUpdateImageAPI.js
@@ -1,85 +1,62 @@
-import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps'
+import { Given } from 'cypress-cucumber-preprocessor/steps';
 
 const userId = 191222447;
 const source = 'testeAPI';
-const color = 'black';
+const color = 'red';
 const productId = 7;
-const authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3d3cuYWR2YW50YWdlb25saW5lc2hvcHBpbmcuY29tIiwidXNlcklkIjoxOTEyMjI0NDcsInN1YiI6InN0cmluZyIsInJvbGUiOiJBRE1JTiJ9.28Ezg3D_namYndEPhj7ClbEdIwxGB-PVSwJWbZ-Dn9M';
-let responseData;
+const authToken =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ3d3cuYWR2YW50YWdlb25saW5lc2hvcHBpbmcuY29tIiwidXNlcklkIjoxOTEyMjI0NDcsInN1YiI6InN0cmluZyIsInJvbGUiOiJBRE1JTiJ9.28Ezg3D_namYndEPhj7ClbEdIwxGB-PVSwJWbZ-Dn9M';
+const queryParams = {
+  product_id: productId
+}
+let responseData = {};
 
-Given("que enviei a requisição de Update para a API", () => {  
-      
-    function form_request(method, url, formData, done) {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-    
-        xhr.setRequestHeader(
-            "Authorization",
-            `Bearer ${authToken}`
-        );
-      
-        xhr.onload = function () {
-            done(xhr); // Passando o xhr completo para o callback
-        };
-      
-        xhr.onerror = function () {
-            done(xhr); // Passando o xhr completo para o callback
-        };
-      
-        xhr.send(formData);
-    }
-    
+Given("que enviei a requisição de Update para a API", () => {
     const fileName = "laptophp.jpg";
-    const method = "POST";
-    const url = `https://www.advantageonlineshopping.com/catalog/api/v1/product/image/${userId}/${source}/${color}?product_id=${productId}`;
-    
-    cy.fixture(fileName, "binary").then((txtBin) => Cypress.Blob.binaryStringToBlob(txtBin)).then((blob) => {
-        const formData = new FormData();
-        formData.append("file", blob, fileName);
-        form_request(method, url, formData, function (xhr) {
 
-            expect(xhr.status).to.eq(200);
-            expect(xhr.responseText).to.not.be.empty;
-            
-            try {
-                responseData = JSON.parse(xhr.responseText);
-                
-                expect(responseData).to.have.property('success');
-                expect(responseData.success).to.be.a('boolean');
-                expect(responseData.success).to.eq(true); 
-                expect(responseData).to.have.property('id');
-                expect(responseData.id).to.be.a('number');
-                expect(responseData).to.contain('testeAPI');
-                expect(responseData.imageId).to.be.a('number');
-                expect(responseData).to.have.property('imageColor');
-                expect(responseData.imageColor).to.be.a('string');
-            } catch (e) {
-                // cy.log('Não foi possível analisar o texto de resposta como JSON');
-                expect(xhr.responseText).to.not.be.empty
-                }
+    cy.fixture(fileName, "binary")
+        .then((txtBin) => Cypress.Blob.binaryStringToBlob(txtBin, 'image/jpeg'))
+        .then((blob) => {
+            var formData = new FormData();
+
+            formData.append("file", blob, fileName);
+
+            cy.log(blob.type)
+
+            cy.request({
+                method: 'POST',
+                url: `https://www.advantageonlineshopping.com/catalog/api/v1/product/image/${userId}/${source}/${color}`,
+                qs: queryParams,
+                log: true,
+                failOnStatusCode: false,
+                headers: {
+                    Authorization: `Bearer ${authToken}`,
+                },
+                body: formData
+            }).then((response) => {
+               const bodyString = String.fromCharCode.apply(null, new Uint8Array(response.body));
+               const parsedBody = JSON.parse(bodyString);
+
+               responseData = {
+                    status: response.status,
+                    body: parsedBody
+               };
+           });
         });
-    });
 })
 
 When("receber o retorno 200", () => {
-
-    if (responseData.status === 200) {
-        expect(responseData.status).to.eq(200);
-    } else {
-        cy.log(`Erro na requisição: Status ${responseData.status}`);
-        cy.log('Response Body:', responseData.body);
-    }
-
-})
+    expect(responseData.status).to.eq(200);
+    cy.log('STATUS ', responseData.status);
+});
 
 Then("eu valido que o produto foi atualizado corretamente", () => {
-    
-    expect(responseData.body).property('success').to.eq('true');
-    
-})
+    expect(responseData.body).to.not.be.empty;
+    cy.log('reason ', responseData.body.reason)
+});
 
 And("eu verifico o novo id da nova imagem inserida", () => {
-
-    expect(responseData).to.have.property('imageColor');
-
-})
+    expect(responseData.body).to.have.property('imageColor');
+    cy.log('New Image ID ', responseData.body.imageId )
+    cy.log('New Image Color ', responseData.body.imageColor)
+});
